e5154f1ef0caafecf2a75363d56ad38f14360ec7
L13L13 <lenaspennrath@gmail.com>
2024-03-01 12:51:43 +0100

    App.js änderung

--- react-foodblog/src/App.js
+++ react-foodblog/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Zutatenliste from "./components/Zutatenliste/Zutatenliste";
 
 import Single_post from "./components/single_post/Single_post";
 
@@ -9,6 +10,9 @@ function App() {
       <header className="App-header">
         <Single_post></Single_post>
       </header>
+      <h1 className ="max-w-xl m-auto bg-organge-100 mt-20">
+        <Zutatenliste />
+      </h1>
     </div>
   );
 }
