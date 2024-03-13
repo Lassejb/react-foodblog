5d56b694c64416f65bb27e0f7bec72db14857094
melirys03 <93533577+melirys03@users.noreply.github.com>
2024-03-13 15:48:35 +0100

    Update Zubereitung.js

--- src/components/Zubereitung/Zubereitung.js
+++ src/components/Zubereitung/Zubereitung.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Zubereitung({ zubereitungsschritte }) {
-  return (
+const zubereitungsschritte = [
+  "Den Backofen auf 180 Grad vorheizen",
+  "Die Knoblauchbutter vorbereiten, indem Sie die weiche Butter, behackten Knoblauch, Petersilie und Salz vermischen",
+  "Die Brötchen halbieren und die Knoblauchbutter großzügig auf beiden Seiten verteilen",
+  "Die Brötchen für etwa 10 Minuten im vorgeheizten Backofen backen, bis sie goldbraun sind",
+  "Herausnehmen und servieren"
+];
+
+function zubereitungsschritte() {
+  return 
     <div>
       <h2>Zubereitung</h2>
       <ol>
@@ -10,8 +18,17 @@ function Zubereitung({ zubereitungsschritte }) {
         ))}
       </ol>
     </div>
+  ;
+}
+
+function Zubereitungschritte() {
+  return (
+    <div id="Zubereitungschritte">
+      <h2>Zubereitung</h2>
+      <Zubereitung zubereitungsschritte={zubereitungsschritte} />
+    </div>
   );
 }
 
-export default Zubereitung;
+export default Zubereitungschritte;
 
