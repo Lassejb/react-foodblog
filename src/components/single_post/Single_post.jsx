d361dc7eb956bcde4e86cd72d571d122565911a3
javi1337 <jan@pulsarstudio.de>
2024-03-14 19:22:18 +0100

    Single Post Picture Update

--- src/components/single_post/Single_post.jsx
+++ src/components/single_post/Single_post.jsx
@@ -5,20 +5,21 @@ import { useParams } from "react-router-dom";
 import Zutatenliste from "../Zutatenliste/Zutatenliste";
 import Zutaten from "../Zutatenliste/Zutaten";
 
-import placeholder_image from "../../assets/placeholder.jpg";
 import jsonData from "../../assets/text.json";
-
 
 const Zubereitung = lazy(() => import("../Zubereitung/Zubereitung"));
 
 export default function Single_post() {
   const { id } = useParams();
-  console.log(jsonData[id].picture)
   return (
     <div className="Single_post">
       <div className="single_post_Box">
         <div className="image_container">
-          <img src={jsonData[id].picture} alt="" className="single_post_Image" />
+          <img
+            src={jsonData[id].picture}
+            alt=""
+            className="single_post_Image"
+          />
         </div>
         <div>
           <h1 className="single_post_title">{jsonData[id].title}</h1>
@@ -26,12 +27,11 @@ export default function Single_post() {
           <p className="single_post_zutaten">{jsonData[id].zutaten}</p>
         </div>
         <div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Zubereitung value={id} />
-        </Suspense>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Zubereitung value={id} />
+          </Suspense>
         </div>
-        <div>
-        </div>
+        <div></div>
       </div>
     </div>
   );
